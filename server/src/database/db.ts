cf917af9949127276b020c9bea6253021d157146
Mangalam Raj <mango.26june@gmail.com>
2024-06-11 13:35:23 +0530

    config

--- server/src/database/db.ts
+++ server/src/database/db.ts
@@ -4,12 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const Connection = async () => {
-  const URL = process.env.MONGODB_URI;
+  const URL = "mongodb+srv://mango:mango123@cluster0.nqhia7c.mongodb.net/";
   const options: ConnectOptions = {
     bufferCommands: false,
   };
   try {
-    await mongoose.connect(URL!, options);
+    await mongoose.connect(
+      "mongodb+srv://mango:mango123@cluster0.nqhia7c.mongodb.net/",
+      options,
+    );
     console.log("Database connected sucessfully");
   } catch (error: any) {
     console.log("error while connecting with the database", error.message);
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,13 @@ import Router from "./routes/route";
 
 const app = express();
 dotenv.config;
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }),
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
